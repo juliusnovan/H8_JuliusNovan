d1536650a5831d26a82c8a4a3e520cb1721f8230
unknown <vandust@gmail.com>
2022-06-04 13:37:57 +0700

    Initial Commit add Sesi 1

---
+++ Sesi-1(Spyder).py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+##integer
+print(10)
+print(type(10))
+
+
+##float
+print(4.2)
+print(type(4.2))
+
+print(4.)
+
+print(.2)
+
+print(.4e7)
+
+print(4.2e-4)
+
+
+##string
+print("Hacktiv8")
+print(type("Hacktiv8"))
+
+#bisa pakai petik satu (')
+print('Hacktiv8')
+print(type('Hacktiv8'))
+
+print("Hack\"tiv8")
+
+print("This string contains a single quote (') character.")
+
+print('This string contains a double quote (") character.')
+
+
+
+##boolean
+print(type(True))
+
+print(type(False))
+
+
+##variable assignment
+n = 300
+print(n)
+
+# Later, if you change the value of n and use it again, the new value will be substituted instead:
+n = 1000
+print(n)
+
+n
+
+
+#latihan
+a = 10
+t = 20
+l = a*t/2
+print(l)
+
+
+a = b = c = 300
+print(a, b, c)
+
+
+##Variable Types in Python
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+
+#Variable Names
+name = "Hacktiv8"
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
+
+
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+
+print(age, Age, aGe, AGE, a_g_e, _age, age_, _AGE_)
+
+
+##Operators and Expressions in Python
+a = 10
+b = 20
+a + b
+
+
+a = 10
+b = 20
+a + b - 5
+
+a = 10
+b = 20
+a + b * 5
+
+
+# Here are some examples of these operators in use:
+
+a = 4
+b = 3
+
+##Arithmetic Operators
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+# The result of standard division (/) is always a float, even if the dividend is evenly divisible by the divisor:
+
+10 / 5
+
+
+##Comparison Operators
+# Here are examples of the comparison operators in use:
+
+a = 10
+b = 20
+print(a == b)
+
+print(a != b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+a = 30
+b = 30
+print(a == b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+##String Manipulation
+# + Operators
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
+
